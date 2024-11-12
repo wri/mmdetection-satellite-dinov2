bd4af819da5294509f4baa161b7883b83b2593cc
John Brandt <john.brandt@wri.org>
2024-11-12 08:51:44 -0500

    Add AdamSPD

--- mmdet/engine/optimizers/__init__.py
+++ mmdet/engine/optimizers/__init__.py
@@ -2,6 +2,7 @@
 from .layer_decay_optimizer_constructor import \
     LearningRateDecayOptimizerConstructor
 from .layer_decay_optimizer_constructor_vit import LayerDecayOptimizerConstructor_ViT
+from .AdamSPD import AdamSPD
 
 
-__all__ = ['LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor_ViT']
+__all__ = ['LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor_ViT', 'AdamSPD']
--- mmdet/engine/optimizers/layer_decay_optimizer_constructor_vit.py
+++ mmdet/engine/optimizers/layer_decay_optimizer_constructor_vit.py
@@ -2,18 +2,9 @@ import json
 from mmengine.dist import get_dist_info
 from mmengine.optim import DefaultOptimWrapperConstructor
 from mmengine.registry import OPTIM_WRAPPER_CONSTRUCTORS
+import copy
+import torch
 
-
-def get_num_layer_for_vit(var_name, num_max_layer):
-    if var_name in ("backbone.cls_token", "backbone.mask_token", "backbone.pos_embed"):
-        return 0
-    elif var_name.startswith("backbone.patch_embed"):
-        return 0
-    elif var_name.startswith("backbone.blocks"):
-        layer_id = int(var_name.split('.')[2])
-        return layer_id + 1
-    else:
-        return num_max_layer - 1
 
 @OPTIM_WRAPPER_CONSTRUCTORS.register_module()
 class LayerDecayOptimizerConstructor_ViT(DefaultOptimWrapperConstructor):
@@ -30,50 +21,35 @@ class LayerDecayOptimizerConstructor_ViT(DefaultOptimWrapperConstructor):
                 submodule of DCN, `is_dcn_module` will be passed to
                 control conv_offset layer's learning rate. Defaults to None.
         """
+
         parameter_groups = {}
-        print(self.paramwise_cfg)
-        num_layers = self.paramwise_cfg.get('num_layers') + 2
-        layer_decay_rate = self.paramwise_cfg.get('layer_decay_rate')
-        print("Build LayerDecayOptimizerConstructor %f - %d" % (layer_decay_rate, num_layers))
-        weight_decay = self.base_wd
 
-        for name, param in module.named_parameters():
-            if not param.requires_grad:
+        #params_anchor = copy.deepcopy(params_to_opt)
+        for i, value in enumerate(module.named_parameters()):
+            if not value[1].requires_grad:
                 continue  # frozen weights
-            if len(param.shape) == 1 or name.endswith(".bias") or 'pos_embed' in name:
-                group_name = "no_decay"
-                this_weight_decay = 0.
+            group_name = value[0]
+
+            this_weight_decay = 0.05 if ('backbone' in value[0]) else 1e-4
+            scale = 1e-1 if (('backbone' in value[0] and 'fpn' not in value[0])) else 1
+            this_weight_decay = this_weight_decay if 'norm' not in value[0] else 0
+            this_weight_decay = this_weight_decay if 'cls_token' not in value[0] else 0
+            this_weight_decay = this_weight_decay if 'dist_token' not in value[0] else 0
+            this_weight_decay = this_weight_decay if 'mask_token' not in value[0] else 0
+            this_weight_decay = this_weight_decay if 'pos_embed' not in value[0] else 0
+            
+            parameter_groups[group_name] = {
+                        'weight_decay': this_weight_decay,
+                        'params': value[1],
+                        'param_names': [],
+                        'pre': [],
+                        'lr_scale': scale,
+                        'name': group_name,
+                        'lr': scale * self.base_lr,
+                    }
+            if 'backbone' in value[0]:
+                parameter_groups[group_name]['pre'].append(copy.deepcopy(value[1]))
             else:
-                group_name = "decay"
-                this_weight_decay = weight_decay
-
-            layer_id = get_num_layer_for_vit(name, num_layers)
-            group_name = "layer_%d_%s" % (layer_id, group_name)
-
-            if group_name not in parameter_groups:
-                scale = layer_decay_rate ** (num_layers - layer_id - 1)
-
-                parameter_groups[group_name] = {
-                    "weight_decay": this_weight_decay,
-                    "params": [],
-                    "param_names": [], 
-                    "lr_scale": scale, 
-                    "group_name": group_name, 
-                    "lr": scale * self.base_lr, 
-                }
-
-            parameter_groups[group_name]["params"].append(param)
-            parameter_groups[group_name]["param_names"].append(name)
-        rank, _ = get_dist_info()
-        if rank == 0:
-            to_display = {}
-            for key in parameter_groups:
-                to_display[key] = {
-                    "param_names": parameter_groups[key]["param_names"], 
-                    "lr_scale": parameter_groups[key]["lr_scale"], 
-                    "lr": parameter_groups[key]["lr"], 
-                    "weight_decay": parameter_groups[key]["weight_decay"], 
-                }
-            #print("Param groups = %s" % json.dumps(to_display, indent=2))
-
+                parameter_groups[group_name]['pre'].append(torch.tensor([0], device = torch.device('cuda')))
         params.extend(parameter_groups.values())
+
